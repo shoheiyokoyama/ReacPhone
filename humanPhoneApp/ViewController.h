40e51eec8c92cbf4d92e81d6abf4b6ee632e81ef
Shohei Yokoyama <yokoyama@talknote.com>
2015-04-21 21:45:03 +0900

    first commit

---
+++ humanPhoneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  humanPhoneApp
+//
+//  Created by 横山祥平 on 2015/04/21.
+//  Copyright (c) 2015年 shohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ humanPhoneApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  humanPhoneApp
+//
+//  Created by 横山祥平 on 2015/04/21.
+//  Copyright (c) 2015年 shohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
